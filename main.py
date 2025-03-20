e36de24092065f562d906775447666593b5fb1a4
ynng1 <yunning_uh@mymail.sutd.edu.sg>
2025-03-20 15:31:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from subscriber import ee, start_subscriber
 
+from smotor import TB6600StepperMotor
 
 import RPi.GPIO as GPIO          
 from time import sleep
@@ -12,6 +13,7 @@ load_dotenv()
 @ee.on("purchase")
 def on_purchase(pots_away):
     print(f"RECIEVED MESSAGE = {pots_away}")
+    TB6600StepperMotor(pulse_pin=20, dir_pin=21, enable_pin=None)
 
 
 
