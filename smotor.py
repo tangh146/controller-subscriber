0e291c40fedfa482289ef09695d540c0ea151179
ynng1 <yunning_uh@mymail.sutd.edu.sg>
2025-03-20 15:18:27 +0800

    Update smotor.py

--- smotor.py
+++ smotor.py
@@ -1,107 +1,123 @@
-# Based on: https://www.raspberrypi.org/forums/viewtopic.php?t=242928\.
-#
-# Software to drive 4 wire stepper motor using a TB6600 Driver
-# PRi - RPi 3B
-#
-# Route 3.3 VDC to the controller "+" input for each: ENA, PUL, and DIR
-#
-# Connect GPIO pins as shown below) to the "-" input for each: ENA, PUL, and DIR
-#
-#
-from time import sleep
 import RPi.GPIO as GPIO
-#
-PUL = 20  # Stepper Drive Pulses
-DIR = 21  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
-#ENA = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
-# DIRI = 14  # Status Indicator LED - Direction
-# ENAI = 15  # Status indicator LED - Controller Enable
-#
-# NOTE: Leave DIR and ENA disconnected, and the controller WILL drive the motor in Default direction if PUL is applied.
-# 
-GPIO.setmode(GPIO.BCM)
-# GPIO.setmode(GPIO.BOARD) # Do NOT use GPIO.BOARD mode. Here for comparison only. 
-#
-GPIO.setup(PUL, GPIO.OUT)
-GPIO.setup(DIR, GPIO.OUT)
-#GPIO.setup(ENA, GPIO.OUT)
-# GPIO.setup(DIRI, GPIO.OUT)
-# GPIO.setup(ENAI, GPIO.OUT)
-#
-print('PUL = GPIO 17 - RPi 3B-Pin #11')
-print('DIR = GPIO 27 - RPi 3B-Pin #13')
-#print('ENA = GPIO 22 - RPi 3B-Pin #15')
-# print('ENAI = GPIO 14 - RPi 3B-Pin #8')
-# print('DIRI = GPIO 15 - RPi 3B-Pin #10')
+import time
 
-#
-print('Initialization Completed')
-#
-# Could have usesd only one DURATION constant but chose two. This gives play options.
-durationFwd = 5000 # This is the duration of the motor spinning. used for forward direction
-durationBwd = 5000 # This is the duration of the motor spinning. used for reverse direction
-print('Duration Fwd set to ' + str(durationFwd))
-print('Duration Bwd set to ' + str(durationBwd))
-#
-delay = 0.0000001 # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
-print('Speed set to ' + str(delay))
-#
-cycles = 1000 # This is the number of cycles to be run once program is started.
-cyclecount = 0 # This is the iteration of cycles to be run once program is started.
-print('number of Cycles to Run set to ' + str(cycles))
-#
-#
-def forward():
-    #GPIO.output(ENA, GPIO.HIGH)
-    #GPIO.output(ENAI, GPIO.HIGH)
-    print('ENA set to HIGH - Controller Enabled')
-    #
-    sleep(.5) # pause due to a possible change direction
-    GPIO.output(DIR, GPIO.LOW)
-    GPIO.output(DIR, GPIO.LOW)
-    print('DIR set to LOW - Moving Forward at ' + str(delay))
-    print('Controller PUL being driven.')
-    for x in range(durationFwd): 
-        GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    #GPIO.output(ENA, GPIO.LOW)
-    #GPIO.output(ENAI, GPIO.LOW)
-    print('ENA set to LOW - Controller Disabled')
-    sleep(.5) # pause for possible change direction
-    return
-#
-#
-def reverse():
-    #GPIO.output(ENA, GPIO.HIGH)
-    #GPIO.output(ENAI, GPIO.HIGH)
-    print('ENA set to HIGH - Controller Enabled')
-    #
-    sleep(.5) # pause due to a possible change direction
-    GPIO.output(DIR, GPIO.HIGH)
-    GPIO.output(DIR, GPIO.HIGH)
-    print('DIR set to HIGH - Moving Backward at ' + str(delay))
-    print('Controller PUL being driven.')
-    #
-    for y in range(durationBwd):
-        GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    #GPIO.output(ENA, GPIO.LOW)
-    #GPIO.output(ENAI, GPIO.LOW)
-    #print('ENA set to LOW - Controller Disabled')
-    sleep(.5) # pause for possible change direction
-    return
+class TB6600StepperMotor:
+    def __init__(self, pulse_pin, dir_pin, enable_pin=None, steps_per_rev=200, microstepping=1):
+        """
+        Initialize the TB6600 stepper motor driver.
+        
+        Args:
+            pulse_pin (int): GPIO pin connected to the PUL+ pin on TB6600
+            dir_pin (int): GPIO pin connected to the DIR+ pin on TB6600
+            enable_pin (int, optional): GPIO pin connected to the ENA+ pin on TB6600
+            steps_per_rev (int): Number of steps per revolution (defaults to 200)
+            microstepping (int): Microstepping setting on the TB6600 (1, 2, 4, 8, 16, or 32)
+        """
+        self.pulse_pin = pulse_pin
+        self.dir_pin = dir_pin
+        self.enable_pin = enable_pin
+        self.steps_per_rev = steps_per_rev
+        self.microstepping = microstepping
+        
+        # Setup GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pulse_pin, GPIO.OUT)
+        GPIO.setup(self.dir_pin, GPIO.OUT)
+        
+        # Set direction to a default value (clockwise in this case)
+        GPIO.output(self.dir_pin, GPIO.HIGH)
+        
+        if self.enable_pin is not None:
+            GPIO.setup(self.enable_pin, GPIO.OUT)
+            # Enable the driver (active low)
+            GPIO.output(self.enable_pin, GPIO.LOW)
+    
+    def enable(self):
+        """Enable the motor driver if an enable pin is connected."""
+        if self.enable_pin is not None:
+            GPIO.output(self.enable_pin, GPIO.LOW)
+    
+    def disable(self):
+        """Disable the motor driver if an enable pin is connected."""
+        if self.enable_pin is not None:
+            GPIO.output(self.enable_pin, GPIO.HIGH)
+    
+    def step(self, steps, delay=0.0005):
+        """
+        Move the motor a specified number of steps.
+        
+        Args:
+            steps (int): Number of steps to move
+            delay (float): Delay between pulses in seconds (controls speed)
+        """
+        for _ in range(steps):
+            GPIO.output(self.pulse_pin, GPIO.HIGH)
+            time.sleep(delay)
+            GPIO.output(self.pulse_pin, GPIO.LOW)
+            time.sleep(delay)
+    
+    def rotate_degrees(self, degrees, delay=0.0005):
+        """
+        Rotate the motor by a specified angle in degrees.
+        
+        Args:
+            degrees (float): The angle to rotate in degrees
+            delay (float): Delay between pulses in seconds (controls speed)
+        """
+        steps = int((degrees / 360.0) * self.steps_per_rev * self.microstepping)
+        self.step(steps, delay)
+    
+    def rotate_revolutions(self, revolutions, delay=0.0005):
+        """
+        Rotate the motor by a specified number of revolutions.
+        
+        Args:
+            revolutions (float): The number of revolutions to rotate
+            delay (float): Delay between pulses in seconds (controls speed)
+        """
+        steps = int(revolutions * self.steps_per_rev * self.microstepping)
+        self.step(steps, delay)
+    
+    def cleanup(self):
+        """Clean up GPIO resources."""
+        if self.enable_pin is not None:
+            self.disable()
+        GPIO.cleanup()
 
-while cyclecount < cycles:
-    forward()
-    reverse()
-    cyclecount = (cyclecount + 1)
-    print('Number of cycles completed: ' + str(cyclecount))
-    print('Number of cycles remaining: ' + str(cycles - cyclecount))
-#
-GPIO.cleanup()
-print('Cycling Completed')
-#
+
+# Example usage
+if __name__ == "__main__":
+    try:
+        # Define pins connected to TB6600
+        PULSE_PIN = 20  # GPIO pin connected to PUL+
+        DIR_PIN = 21    # GPIO pin connected to DIR+
+        
+        # Create stepper motor instance (adjust parameters as needed)
+        # Common values for steps_per_rev: 200 (1.8° motor) or 400 (0.9° motor)
+        # Set microstepping according to your TB6600 switch settings (1, 2, 4, 8, 16, 32)
+        motor = TB6600StepperMotor(
+            pulse_pin=PULSE_PIN,
+            dir_pin=DIR_PIN,
+            enable_pin=None,  # Not using the enable pin
+            steps_per_rev=200,
+            microstepping=16
+        )
+        
+        print("Running motor for 1 revolution slowly...")
+        motor.rotate_revolutions(1, delay=0.001)
+        
+        time.sleep(1)
+        
+        print("Running motor for 180 degrees quickly...")
+        motor.rotate_degrees(180, delay=0.0003)
+        
+        time.sleep(1)
+        
+        print("Running motor for 1000 steps...")
+        motor.step(1000, delay=0.0005)
+        
+    except KeyboardInterrupt:
+        print("\nProgram stopped by user")
+    finally:
+        if 'motor' in locals():
+            motor.cleanup()
